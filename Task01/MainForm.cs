c9ffafda8be0ac192aaa22eb2e58e8a7176070d8
LPA <p.luchshev@khai.edu>
2024-09-30 14:14:18 +0300

    First step: - Task #01 - Report template

---
+++ Task01/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using static Task01.OpenGL;
+
+
+namespace Task01
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
